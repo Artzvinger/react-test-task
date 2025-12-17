8263544174bf06e4c9f26da3684085ae001b72e6
Artzvinger <i040104k@gmail.com>
2025-12-17 14:35:16 +0300

    Поиск и модальное окно

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from './products'
 
 function App() {
+	const [search, setSearch] = useState('')
+	const [selectedProduct, setSelectedProduct] = useState(null)
+
 	return (
 		<div>
 			<h1>Список товаров</h1>
-
+			<input
+				type='text'
+				placeholder='Поиск товаров...'
+				value={search}
+				onChange={function (e) {
+					setSearch(e.target.value)
+				}}
+			/>
 			<div>
-				{products.map(function (item) {
-					return (
-						<div key={item.id}>
-							<img src={item.image} alt={item.title} width='150' />
-							<h3>{item.title}</h3>
-							<p>Цена: {item.price} ₽</p>
-						</div>
-					)
-				})}
+				{products
+					.filter(function (item) {
+						return item.title.toLowerCase().includes(search.toLowerCase())
+					})
+					.map(function (item) {
+						return (
+							<div
+								key={item.id}
+								onClick={function () {
+									setSelectedProduct(item)
+								}}
+							>
+								<img src={item.image} alt={item.title} width='150' />
+								<h3>{item.title}</h3>
+								<p>Цена: {item.price} ₽</p>
+							</div>
+						)
+					})}
 			</div>
+
+			{selectedProduct && (
+				<div onClick={function (e) {e.stopPropagation()}}>
+					<button onClick={function () {setSelectedProduct(null)}}>X</button>
+					<img
+						src={selectedProduct.image}
+						alt={selectedProduct.title}
+						width='150'
+					/>
+					<h2>{selectedProduct.title}</h2>
+					<p>{selectedProduct.description}</p>
+					<p>Цена: {selectedProduct.price} ₽</p>
+					<button>Купить</button>
+				</div>
+			)}
 		</div>
 	)
 }
